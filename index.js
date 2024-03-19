e3cc310758fc4baff01c545d09f8e8d8ed73edaf
m11toh <martohmashah@gmail.com>
2024-03-19 17:57:39 +0300

    max

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ console.log(multiply)
 // random
  let random =  Math.floor(Math.random() * 10) + 1;
 
- 
+// mod
+ let num3 = 54
+ let num4 = 5
+
+ const mod= num3 % num4
+ console.log(mod)
+
+ //max
+let max = Math.max(11, 12, 13, 14, 15, 16, 17, 18, 19, 20);

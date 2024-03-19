57029efcfdc47f360764d005fea7ac963c9b888a
m11toh <martohmashah@gmail.com>
2024-03-19 15:48:28 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2 
+console.log(multiply)
+
+// random
+ let random =  Math.floor(Math.random() * 10) + 1;
+
+ 
